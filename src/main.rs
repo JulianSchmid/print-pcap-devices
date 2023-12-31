1e2b62ee140c334116c155e2f99d7811f39fa4aa
Julian Schmid <info@julianschmid.name>
2023-12-31 15:57:49 +0100

    Applied rust fmt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     // list all of the devices pcap tells us are available
     for device in pcap::Device::list().expect("device lookup failed") {
         println!("Found device! {:?}", device);
-        
+
         let Ok(cap) = pcap::Capture::from_device(device) else {
             println!("    Error creating capture");
             continue;
@@ -14,7 +14,12 @@ fn main() {
 
         {
             let l = cap.get_datalink();
-            println!("  Main data link: {:?} {:?} {:?}", l, l.get_name(), l.get_description())
+            println!(
+                "  Main data link: {:?} {:?} {:?}",
+                l,
+                l.get_name(),
+                l.get_description()
+            )
         }
 
         if let Ok(links) = cap.list_datalinks() {
